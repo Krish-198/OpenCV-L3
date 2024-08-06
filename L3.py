8a4423f2928d8cce62acf7f9c9335017ef9b0625
Krish Sethi <95207265+Krish-198@users.noreply.github.com>
2024-08-06 19:13:33 +0530

    Update L3.py

--- L3.py
+++ L3.py
@@ -15,35 +15,22 @@ for i in range(row):
 cv2.imshow("Blah 1",img2)
 cv2.waitKey(0)
 
+b=cv2.getRotationMatrix2D((col/2,row/2),45,1)
+r=cv2.warpAffine(img2,b,(col,row))
+
+cv2.imwrite("Image.png",r)
+cv2.waitKey(0)
 
 
+#Edge Dectetion
+h=cv2.Canny(img,100,200)
+cv2.imwrite("Tomato(2).png",h)
+cv2.waitKey(0)
 
 
+#hsv
+hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+cv2.imshow("Gray Image",hsv)
+cv2.waitKey(0)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-cv2.destroyAllWindows()
